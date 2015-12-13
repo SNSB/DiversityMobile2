eae903e4fafdab46303a3d18433e9c0e408fb96a
Georg Rollinger <rthundr@live.de>
2015-12-13 21:38:13 +0100

    React Hello World

---
+++ DiversityMobile2/gulpfile.js
@@ -0,0 +1,25 @@
+/// <binding BeforeBuild='scripts' />
+var gulp = require('gulp'),
+	browserify = require('browserify'),
+    buffer = require('vinyl-buffer'),
+    source = require('vinyl-source-stream'),
+	sourcemaps = require('gulp-sourcemaps'),
+    reactify = require('reactify')
+;
+	
+gulp.task('scripts', function () {
+    var b = browserify({
+        entries: './typescript/index.js',
+        debug: true,
+        transform: [reactify]
+    });
+
+    b.bundle()
+        .pipe(source('index.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('./www/scripts/'));
+});
+
+gulp.task('default', ['scripts']);
